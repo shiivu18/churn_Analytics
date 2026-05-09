29a454c8f54560b3a4dac59876f91fe22537d1db
Shiivu18 <shokdarshivum@gmail.com>
2026-05-09 20:56:28 +0530

    make pridictions

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import joblib
+import pandas as pd
+
+model_columns = joblib.load(
+    r"D:\Churn-Analytics\models\model_columns.pkl"
+)
 
 st.title("Customer Churn Prediction App")
 
@@ -24,4 +29,31 @@ monthly_charge = st.number_input(
 
 
 if st.button("Predict"):
-    st.write("Prediction system ready")
+
+    # Create input dataframe
+    input_data = pd.DataFrame({
+        'Tenure Months': [tenure],
+        'Monthly Charge': [monthly_charge]
+    })
+
+    # Add missing columns
+    for col in model_columns:
+        if col not in input_data.columns:
+            input_data[col] = 0
+
+    # Arrange columns correctly
+    input_data = input_data[model_columns]
+
+    # Make prediction
+    prediction = model.predict(input_data)
+
+    # Prediction result
+    if prediction[0] == 1:
+        st.error("Customer is likely to churn")
+    else:
+        st.success("Customer is likely to stay")
+
+
+
+
+        
